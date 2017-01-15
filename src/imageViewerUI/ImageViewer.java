0f38ce9a52ab3a363742fee69922cffdeb19fda4
Jean Paul Iradukunda <jp.iradukunda@pesachoice.com>
2017-01-15 10:18:08 -0600

    Initial commit to the image viewer project

---
+++ src/imageViewerUI/ImageViewer.java
@@ -0,0 +1,43 @@
+package imageViewerUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ImageViewer extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ImageViewer frame = new ImageViewer();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ImageViewer() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("JP - Image Viewer");
+		setBounds(150, 50, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
